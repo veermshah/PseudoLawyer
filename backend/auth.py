28ea302aba6e9852ef996f048d5a982ae2c5568c
Kushagra Bharti <kushagrabharti@gmail.com>
2024-09-24 23:29:11 -0500

    testing AWS cognito

--- backend/auth.py
+++ backend/auth.py
@@ -8,8 +8,8 @@ cognito_client = boto3.client('cognito-idp')
 def login():
     data = request.get_json()
     response = cognito_client.admin_initiate_auth(
-        UserPoolId='us-east-1_XXXXXXXXX',
-        ClientId='XXXXXXXXXXXXXXXXXXXXXXXXXX',
+        UserPoolId='env.AWS_COGNITO_USER_POOL_ID',
+        ClientId='env.AWS_COGNITO_APP_CLIENT_ID',
         AuthFlow='ADMIN_NO_SRP_AUTH',
         AuthParameters={
             'USERNAME': data['username'],
